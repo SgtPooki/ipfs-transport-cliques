ae25fef716572dc0cbe5c62936a2ef4179462c09
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-05-06 16:10:17 -0700

    chore: remove unused

--- src/utils/is-browser-dialable.ts
+++
@@ -1,5 +0,0 @@
-import type { MultiaddrInfo } from './multiaddr-info.js'
-
-export function isBrowserDialable (multiaddrInfo: MultiaddrInfo): boolean {
-  return multiaddrInfo.http ?? multiaddrInfo.https ?? multiaddrInfo.ws ?? multiaddrInfo.wss ?? multiaddrInfo.webtransport ?? multiaddrInfo.webrtc ?? multiaddrInfo.wrtcDirect ?? false
-}
